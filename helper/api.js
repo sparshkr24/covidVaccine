d0f50b9b1c8626d80356948767840a6ab8ea176c
Yash Sahu <itsyashsahu@gmail.com>
2023-06-17 17:04:12 +0530

    Trying to deploy

--- helper/api.js
+++ helper/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://covid-vaccine-two.vercel.app/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
