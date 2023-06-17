b21c5306bcc554f755e64d62493042c3d2315c2f
Sparsh Kumar <shivendra1822@gmail.com>
2023-06-17 13:54:51 +0530

    added production url

--- helper/api.js
+++ helper/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://covid-vaccine-nine.vercel.app/api',
 });
 
 export const setAuthToken = (token) => {
