8fdca14537102706073d0c95081f8db372dc82c5
Sparsh Kumar <shivendra1822@gmail.com>
2023-06-17 14:49:16 +0530

    fixed deployment issues

--- helper/api.js
+++ helper/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://covid-vaccine-beta.vercel.app/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 api.interceptors.request.use((config) => {
