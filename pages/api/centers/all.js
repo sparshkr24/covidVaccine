217f875afd3a8db2554446aa1e27845616fcb951
Sparsh Kumar <shivendra1822@gmail.com>
2023-06-17 15:39:06 +0530

    testing something2

--- pages/api/centers/all.js
+++ pages/api/centers/all.js
@@ -1,33 +1,41 @@
-import prisma from '../../../prisma/prisma';
-import auth from '../../../middleware/auth';
+import prisma from "../../../prisma/prisma";
+import auth from "../../../middleware/auth";
 
-const centersAll = async (req, res) =>{
+let t = 0;
+const centersAll = async (req, res) => {
+  t = 1;
   try {
-    if (req.method == 'DELETE') {
-      const {centerId} = req.query;
-
+    t = 2;
+    if (req.method == "DELETE") {
+      t = 3;
+      const { centerId } = req.query;
+      t = 4;
       if (!centerId) {
-        return res.status(400).json({ message: 'centerId is required in the request body' });
+        return res
+          .status(400)
+          .json({ message: "centerId is required in the request body" });
       }
-      console.log('centerId: ', centerId);
+      console.log("centerId: ", centerId);
+      t = 5;
       await prisma.VaccinationCenter.delete({
         where: {
           id: centerId,
         },
       });
-
-      return res.status(200).json({ message: 'Record deleted successfully' });
+      t = 6;
+      return res.status(200).json({ message: "Record deleted successfully" });
     }
-
+    t = 7;
     if (Object.keys(req.query).length === 0) {
+      t = 8;
       const vaccinationCenters = await prisma.VaccinationCenter.findMany();
       return res.status(200).json(vaccinationCenters);
     }
-
+    t = 9;
     const page = parseInt(req.query.page) || 1;
     const limit = 10; // Number of items per page
     const offset = (page - 1) * limit;
-
+    t = 10;
     const vaccinationCenters = await prisma.VaccinationCenter.findMany({
       take: limit,
       skip: offset,
@@ -36,8 +44,8 @@ const centersAll = async (req, res) =>{
     return res.status(200).json(vaccinationCenters);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: `Internal Server Error T: ${t}` });
   }
-}
+};
 
-export default centersAll
+export default centersAll;
