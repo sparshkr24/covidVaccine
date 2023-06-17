6e18d6436f4a773615574d620891f69cd6040224
Sparsh Kumar <shivendra1822@gmail.com>
2023-06-17 15:10:48 +0530

    production

--- helper/api.js
+++ helper/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://covid-vaccine-two.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
